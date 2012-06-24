727e8aa908191004bfd10def610479233d277ddc
Slava <vs.jacobson@gmail.com>
2012-06-24 17:24:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetThePicture::Application.config.secret_token = '869ff6fba4d1a2e0b0603444fe4213d47dbe60fe77277d7f5e190e619d3cdcbcff7b3cf9a2bdfd25ffe1d01c484abe2c1441a3de12e25831d72a4b2fc3d4c291'
